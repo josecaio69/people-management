aae8569fbd3b35c16ec6420695707f983c098de5
josecaio69 <71333958+josecaio69@users.noreply.github.com>
2022-04-16 15:19:45 -0300

    Add Person Converter DTO and Data a vice-versa

--- src/main/java/br/com/peoplemanagement/converter/PersonConverter.java
+++ src/main/java/br/com/peoplemanagement/converter/PersonConverter.java
@@ -7,7 +7,15 @@ import java.util.function.Function;
 
 public class PersonConverter extends Converter<PersonData, PersonDto> {
 
-    protected PersonConverter(Function<PersonData, PersonDto> fromDto, Function<PersonDto, PersonData> fromEntity) {
+    public PersonConverter(Function<PersonData, PersonDto> fromDto, Function<PersonDto, PersonData> fromEntity) {
         super(fromDto, fromEntity);
+    }
+
+    public PersonDto converterDataFromDto(PersonData person) {
+        return new PersonDto(person.getUID(), person.getFirstName(), person.getLastName(), person.getCpf(), person.getEmail(), person.getBirthData());
+    }
+
+    public PersonData converterDtoFromData(PersonDto person) {
+        return new PersonData(person.getUID(), person.getFirstName(), person.getLastName(), person.getCpf(), person.getEmail(), person.getBirthData());
     }
 }
